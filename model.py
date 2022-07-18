8e2e8e133d2e19a249ca047001486042f52eaf8a
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-07-18 12:21:51 +0200

    u

--- model.py
+++ model.py
@@ -14,7 +14,9 @@ class gaze_network(nn.Module):
     def forward(self, x):
         feature = self.gaze_network(x)
         feature = feature.view(feature.size(0), -1)
-        gaze, head = self.gaze_fc(feature)
+        feature = self.gaze_fc(feature)
+        gaze = feature[:,:2]
+        head = feature [:,2:]
         print(feature)
         print(gaze)
         print(head)
