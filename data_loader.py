8647dab8d9d582c15f1238b23481a9d2795211f4
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-09-22 19:09:04 +0200

    u

--- data_loader.py
+++ data_loader.py
@@ -13,7 +13,7 @@ trans_train = transforms.Compose([
         transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225]),
-        transforms.Resize(size=(128,128)),
+        transforms.Resize(size=(224,224)),
     ])
 
 trans = transforms.Compose([
@@ -21,7 +21,7 @@ trans = transforms.Compose([
         transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225]),
-        transforms.Resize(size=(128,128)),
+        transforms.Resize(size=(224,224)),
     ])
 
 
