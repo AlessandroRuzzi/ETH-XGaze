52f7525af211ad16e393d215ca095ef7f43c9d41
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-10-17 23:08:44 +0200

    u

--- new_data_loader.py
+++ new_data_loader.py
@@ -20,10 +20,113 @@ trans = transforms.Compose([
 
 trans_eval = transforms.Compose(
     [
+        transforms.ToPILImage(),
+        transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         transforms.Resize(size=(224,224))
     ]
 )
+
+def estimateHeadPose(landmarks, face_model, camera, distortion, iterate=True):
+    ret, rvec, tvec = cv2.solvePnP(
+        face_model, landmarks, camera, distortion, flags=cv2.SOLVEPNP_EPNP
+    )
+
+    ## further optimize
+    if iterate:
+        ret, rvec, tvec = cv2.solvePnP(
+            face_model, landmarks, camera, distortion, rvec, tvec, True
+        )
+
+    return rvec, tvec
+
+def normalize(
+    image, camera_matrix, camera_distortion, face_model_load, landmarks, img_dim
+    ):
+        landmarks = np.asarray(landmarks)
+
+        # load face model
+        landmark_use = [20, 23, 26, 29, 15, 19]  # we use eye corners and nose conners
+        face_model = face_model_load[landmark_use, :]
+        facePts = face_model.reshape(6, 1, 3)
+        landmarks_sub = landmarks[[36, 39, 42, 45, 31, 35], :]
+        landmarks_sub = landmarks_sub.astype(
+            float
+        )  # input to solvePnP function must be float type
+        landmarks_sub = landmarks_sub.reshape(
+            6, 1, 2
+        )  # input to solvePnP requires such shape
+        hr, ht = estimateHeadPose(landmarks_sub, facePts, camera_matrix, camera_distortion)
+
+        # data normalization method
+        img_normalized, landmarks_normalized = normalizeData_face(
+            image, face_model, landmarks_sub, hr, ht, camera_matrix, img_dim
+        )
+
+        return img_normalized
+
+def normalizeData_face(img, face_model, landmarks, hr, ht, cam, img_dim):
+            ## normalized camera parameters
+        focal_norm = 960  # focal length of normalized camera
+        distance_norm = 600  # normalized distance between eye and camera
+        roiSize = (img_dim, img_dim)  # size of cropped eye image
+
+        ## compute estimated 3D positions of the landmarks
+        ht = ht.reshape((3, 1))
+        hR = cv2.Rodrigues(hr)[0]  # rotation matrix
+        Fc = np.dot(hR, face_model.T) + ht  # rotate and translate the face model
+        two_eye_center = np.mean(Fc[:, 0:4], axis=1).reshape((3, 1))
+        nose_center = np.mean(Fc[:, 4:6], axis=1).reshape((3, 1))
+        # get the face center
+        face_center = np.mean(
+            np.concatenate((two_eye_center, nose_center), axis=1), axis=1
+        ).reshape((3, 1))
+
+        ## ---------- normalize image ----------
+        distance = np.linalg.norm(
+            face_center
+        )  # actual distance between eye and original camera
+
+        z_scale = distance_norm / distance
+        cam_norm = np.array(
+            [  # camera intrinsic parameters of the virtual camera
+                [focal_norm, 0, roiSize[0] / 2],
+                [0, focal_norm, roiSize[1] / 2],
+                [0, 0, 1.0],
+            ]
+        )
+        S = np.array(
+            [  # scaling matrix
+                [1.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0],
+                [0.0, 0.0, z_scale],
+            ]
+        )
+
+        hRx = hR[:, 0]
+        forward = (face_center / distance).reshape(3)
+        down = np.cross(forward, hRx)
+        down /= np.linalg.norm(down)
+        right = np.cross(down, forward)
+        right /= np.linalg.norm(right)
+        R = np.c_[right, down, forward].T  # rotation matrix R
+
+        W = np.dot(
+            np.dot(cam_norm, S), np.dot(R, np.linalg.inv(cam))
+        )  # transformation matrix
+
+        img_warped = cv2.warpPerspective(img, W, roiSize)  # warp the input image
+
+        # head pose after normalization
+        hR_norm = np.dot(R, hR)  # head pose rotation matrix in normalized space
+        hr_norm = cv2.Rodrigues(hR_norm)[0]  # convert rotation matrix to rotation vectors
+
+        # normalize the facial landmarks
+        num_point = landmarks.shape[0]
+        landmarks_warped = cv2.perspectiveTransform(landmarks, W)
+        landmarks_warped = landmarks_warped.reshape(num_point, 2)
+
+        return img_warped, landmarks_warped
 
 
 def get_train_loader(data_dir,
@@ -113,6 +216,18 @@ class GazeDataset(Dataset):
         self.hdf = None
         self.transform = transform
 
+        self.cam_matrix = []
+        self.cam_distortion = []
+        
+        for cam_id in range(18):
+            cam_file_name = "cam/cam" + str(cam_id).zfill(2) + ".xml"
+            fs = cv2.FileStorage(cam_file_name, cv2.FILE_STORAGE_READ)
+            self.cam_matrix.append(fs.getNode("Camera_Matrix").mat())
+            self.cam_distortion.append(fs.getNode("Distortion_Coefficients").mat())
+            fs.release()
+
+        self.face_model_load = np.loadtxt("face_model.txt")
+
     def __len__(self):
         return len(self.idx_to_kv)
 
@@ -140,13 +255,25 @@ class GazeDataset(Dataset):
         image = self.transform(image)
 
         face_mask = self.hdf_nerf["head_mask"][idx, :]
+        ldms  = self.hdf_nerf["facial_landmarks"][idx, :]
+        cam_ind = self.hdf_nerf["cam_index"][idx, :]
+
         kernel_2 = np.ones((3, 3), dtype=np.uint8)
         face_mask = cv2.erode(face_mask, kernel_2, iterations=2)
-
         face_mask = torch.from_numpy(face_mask)
         nonhead_mask = face_mask < 0.5
         nonhead_mask_c3b = nonhead_mask.expand(3, -1, -1)
         image[nonhead_mask_c3b] = 1.0
+
+        image = normalize(
+                image.detach().cpu().numpy() * 255).astype(
+                    np.uint8,
+                self.cam_matrix[cam_ind],
+                self.cam_distortion[cam_ind],
+                self.face_model_load,
+                ldms,
+                224,
+        )
 
         image = trans_eval(image)
 
