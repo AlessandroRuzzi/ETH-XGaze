7cdd74c48da3e39a81d3591177ab441771ae1384
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-11-01 19:32:01 +0100

    u

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
 
-from modules import resnet50
+from modules import resnet50, resnet18
 
 class gaze_network(nn.Module):
     def __init__(self, use_face=False, num_glimpses=1):
         super(gaze_network, self).__init__()
-        self.gaze_network = resnet50(pretrained=True)
+        self.gaze_network = resnet18(pretrained=True)
 
         self.gaze_fc = nn.Sequential(
             nn.Linear(2048, 4),
