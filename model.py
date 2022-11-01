71ed1a8f6f4b7e366d9ce1833443fc8b4f9a8868
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-11-01 20:03:05 +0100

    u

--- model.py
+++ model.py
@@ -8,15 +8,16 @@ class gaze_network(nn.Module):
         super(gaze_network, self).__init__()
         self.gaze_network = resnet18(pretrained=True)
 
+        #self.gaze_fc = nn.Sequential(
+        #    nn.Linear(2048, 4),
+        #)
         self.gaze_fc = nn.Sequential(
-            nn.Linear(2048, 4),
+            nn.Linear(512, 4),
         )
 
     def forward(self, x):
         feature = self.gaze_network(x)
-        print(feature.shape)
         feature = feature.view(feature.size(0), -1)
-        print(feature.shape)
         feature = self.gaze_fc(feature)
         gaze = feature[:,:2]
         head = feature [:,2:]
