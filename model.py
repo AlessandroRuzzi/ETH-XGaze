34502949f2b6c0c6228ee50b953ffaa4beee1ce1
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-11-01 20:01:14 +0100

    u

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+from xml.sax.handler import feature_external_ges
 import torch.nn as nn
 
 from modules import resnet50, resnet18
@@ -5,7 +6,7 @@ from modules import resnet50, resnet18
 class gaze_network(nn.Module):
     def __init__(self, use_face=False, num_glimpses=1):
         super(gaze_network, self).__init__()
-        self.gaze_network = resnet18(pretrained=True)
+        self.gaze_network = resnet50(pretrained=True)
 
         self.gaze_fc = nn.Sequential(
             nn.Linear(2048, 4),
@@ -13,7 +14,9 @@ class gaze_network(nn.Module):
 
     def forward(self, x):
         feature = self.gaze_network(x)
+        print(feature.shape)
         feature = feature.view(feature.size(0), -1)
+        print(feature.shape)
         feature = self.gaze_fc(feature)
         gaze = feature[:,:2]
         head = feature [:,2:]
